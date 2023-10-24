d51afaaadcff219921c4add7982192c0487ddd3a
Abel Canizares <Abel.Canizares@derivco.co.uk>
2023-10-24 17:10:35 +0200

    first commit

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// with object destructuring
+
+function CoreConcept2({ image, title, description }) {
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+
+}
+
+function CoreConcepts(props) {
+    return (
+      <li>
+        <img src={props.image} alt={props.title} />
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </li>
+    );
+  }
+
+  export default CoreConcepts;
